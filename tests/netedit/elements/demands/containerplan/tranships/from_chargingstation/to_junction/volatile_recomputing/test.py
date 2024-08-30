91a5eb89e3de4457655a007e5b6a7a24a022e942
Pablo Alvarez Lopez <pablo.alvarezlopez@dlr.de>
2024-08-30 14:57:52 +0200

    tests

--- tests/netedit/elements/demands/containerplan/tranships/from_chargingstation/to_junction/volatile_recomputing/test.py
+++ tests/netedit/elements/demands/containerplan/tranships/from_chargingstation/to_junction/volatile_recomputing/test.py
@@ -27,7 +27,7 @@ import neteditTestFunctions as netedit  # noqa
 
 # Open netedit
 neteditProcess, referencePosition = netedit.setupAndStart(
-    neteditTestRoot, ['--sidewalk.guess', '--crossings.guess'])
+    neteditTestRoot, ['--sidewalks.guess', '--crossings.guess'])
 
 # show demand elements in network mode
 netedit.changeEditMode(netedit.attrs.modes.network.showDemandElements)
